d6b66dd27880a48c7e81af8623447201cd5751e2
prevostyohann <prevost.yohann@yahoo.fr>
2025-04-30 18:11:25 +0200

    landing ok, register pro client ok, css (responsive bancale)

--- src/Controller/UserRegistrationController.php
+++ src/Controller/UserRegistrationController.php
@@ -2,43 +2,52 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\UserRegistrationFormType;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\UserRegistrationFormType;
+use App\Entity\User;
 
 class UserRegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
+    #[Route('/register/{type}', name: 'app_register')]
     public function register(
         Request $request,
+        UserPasswordHasherInterface $passwordHasher,
         EntityManagerInterface $entityManager,
-        UserPasswordHasherInterface $passwordHasher
-    ): Response {
+        string $type = 'client'
+    ) {
         $user = new User();
+
         $form = $this->createForm(UserRegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Récupération et hashage du mot de passe
             $plainPassword = $form->get('plainPassword')->getData();
-
             $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
             $user->setPassword($hashedPassword);
+
+            // Attribution du rôle en fonction du type
+            if ($type === 'pro') {
+                $user->setRoles(['ROLE_PRO']);
+            } else {
+                $user->setRoles(['ROLE_CLIENT']);
+            }
 
             $entityManager->persist($user);
             $entityManager->flush();
 
-            $this->addFlash('success', 'Compte créé avec succès !');
+            $this->addFlash('success', 'Inscription réussie !');
 
-            return $this->redirectToRoute('app_login'); // ou une autre route de ton choix
+            return $this->redirectToRoute('app_landing');
         }
 
         return $this->render('user_registration/index.html.twig', [
-            'registrationForm' => $form->createView(),
+            'registrationForm' => $form,
         ]);
     }
 }
